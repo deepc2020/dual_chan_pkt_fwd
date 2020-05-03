0adf9429bb5e71d33c778ce9b9b677fb65845e71
deepc2020 <56347351+deepc2020@users.noreply.github.com>
2020-05-04 00:17:17 +0200

    Added Gateway ID output

--- ha/run.sh
+++ ha/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+mac=$( ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+echo Gateway ID: ${mac:0:8}:ff:ff:${mac:9:12}
+
 cd /home/pi/dual_chan_pkt_fwd/
 ./dual_chan_pkt_fwd
